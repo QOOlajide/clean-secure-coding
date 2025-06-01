6a66cb373bd480cd055459bf0ea0f448a894a60a
Quamdeen Olajide <154437904+QOOlajide@users.noreply.github.com>
2025-06-01 19:24:56 -0400

    Update every_three.py

--- every_three.py
+++ every_three.py
@@ -1,13 +1,19 @@
 #Here, my intention is to print every three nums once being provided with a start parameter. So long as the parameter is less no more than 100, I can return a list
 #Let's think if this is the most efficient example
 
+#def every_three_nums(start):
+  #if start > 100:
+    #return []
+  #my_list = []
+  #for i in range(start, 101, 3):
+    #my_list.append(i)
+  #return my_list
+#Can easily do the following below: 
 def every_three_nums(start):
-  if start > 100:
-    return []
-  my_list = []
-  for i in range(start, 101, 3):
-    my_list.append(i)
-  return my_list
+  return list(range(start, 101, 3))
+#Above demonstrates a simple one-liner which returns the series of numbers derived from range which increment start by 3 up until 101 (exclusive), then converts
+#those series of numbers into a list using the list function. How efficient!
+
 
 #Uncomment the line below when your function is done
 print(every_three_nums(91))
