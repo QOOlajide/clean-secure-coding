00d1346a1a85d9732d0c12509beadf86e9d5bb8d
Quamdeen Olajide <154437904+QOOlajide@users.noreply.github.com>
2025-06-02 13:09:40 -0400

    Create remove_middle_eff.py

---
+++ remove_middle_eff.py
@@ -0,0 +1,3 @@
+#Here's the updated version of my remove_middle python file covered in my coding challenges repository
+def remove_middle(my_list, start, end):
+  return my_list[:start] + my_list[end+1:]
